80a9a88787aa76ed33e8a4b85cc161d273caebac
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2021-08-03 14:03:03 +0530

    Update points.js

--- points/points.js
+++ points/points.js
@@ -2,7 +2,8 @@ module.exports = {
   
   name: "checkpoints",
   code: `$title[Check Points!]
-$description[You have $getGlobalUserVar[points;$authorID] Points!]
+$description[<@$mentioned[1]> has $getGlobalUserVar[points;$mentioned[1]] Points!]
+$argsCheck[1;To Use: \`$getServerVar[prefix]checkpoints <@user> \`]]
 $color[$random[0;999999]]
 `
 }
